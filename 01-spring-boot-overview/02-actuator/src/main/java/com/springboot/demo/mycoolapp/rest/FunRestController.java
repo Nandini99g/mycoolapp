209f01d16251137a8fcf595a24b83c2330513067
Nandini Gadambli <nandinigadambli@gmail.com>
2025-02-24 11:09:58 +0530

    New update git add .git add .

--- 01-spring-boot-overview/02-actuator/src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/02-actuator/src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -9,7 +9,7 @@ public class FunRestController {
   // expose "/" that returns "Hello World!"
 	@GetMapping("/")
 	public String sayHello(){
-		return "Hello World!";
+		return "Hello World!!!!";
 	}
 	
 	//expose a endpoint for "/Workout"
